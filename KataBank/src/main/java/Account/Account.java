1327a0684e7e9e3b1ce89516db5e38916b9f68c9
Juanes07 <juanesdm3@gmail.com>
2022-04-19 14:56:03 -0500

    Se hace correcion metodo Main. se agregan peticiones pedidas en el ejercicio.

--- KataBank/src/main/java/Account/Account.java
+++ KataBank/src/main/java/Account/Account.java
@@ -1,30 +1,22 @@
 package Account;
 
-import java.text.Format;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 public class Account {
 
-    double balance = 0;
-    double debit = 0;
+    double totalBalance = 0;
     String date = "";
 
     public void deposit(double amount, String date) {
-        balance += amount;
+        totalBalance += amount;
         this.date = date;
     }
-    public void withdraw(double amount){
-        balance -= amount;
+    public void withdraw(double amount,  String date){
+        totalBalance -= amount;
     }
-    public void printStatements(double balance, double debit) {
-
+    public void printStatements(double credit,double debit) {
+        totalBalance = credit - debit;
         System.out.println("Este es su estado de cuenta:"
-                +"\n"+  "debit: " +debit
-                +"\n"+  "balance: " + balance );
+                +"\n" + "Credit: " + credit + " || 10/01/2021 || 13/01/2021"
+                +"\n" + "Debit: " + debit + " || 14/01/2021 "
+                +"\n"+  "Balance: " + totalBalance);
     }
-    public double getBalance() {
-        return balance;
-    }
-
 }
--- KataBank/src/main/java/Main/Main.java
+++ KataBank/src/main/java/Main/Main.java
@@ -1,42 +1,18 @@
 package Main;
 
 import Account.Account;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner;
-        scanner = new Scanner(System.in);
-        double retirar = 0;
-        double deposito = 0;
-        String fecha = "";
+        /**
+         * Creacion Objeto account de tipo Account
+         */
         Account account;
         account =new Account();
-        int opcion;
-        do {
-            System.out.println("Eligir transaccion a realizar: "+
-                    "\n" +"1. Depositar dinero"+
-                    "\n" + "2. Retirar Dinero"+
-                    "\n" + "3.Consultar saldo "+
-                    "\n"+ "4.salir");
-            opcion = scanner.nextInt();
-            switch (opcion) {
-                case 1: System.out.println("Ingrese la cantidad de dinero a depositar");
-                    deposito = scanner.nextDouble();
-                    account.deposit(deposito,"10/01/2021");
-                    System.out.println("Ingresaste: " + deposito);
-                    break;
-                case 2:
-                    System.out.println("Ingrese la cantidad de dinero a retirar");
-                    retirar = scanner.nextDouble();
-                    account.withdraw(retirar);
-                    break;
-                case 3:
-                    account.printStatements(account.getBalance(), retirar);
-                default: System.out.println();
-            }
-        }
-        while ( opcion < 4);
-            System.out.println("Saliste del servicio");
+
+        account.deposit(1000,("10/01/2021"));
+        account.deposit(2000, ("13/01/2021"));
+        account.withdraw(500, "14/01/2021");
+        account.printStatements(3000, 500);
     }
 }
