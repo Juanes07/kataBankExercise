d47b049ae210dafd717b5db570d7a562c53c3546
Juanes07 <juanesdm3@gmail.com>
2022-05-12 19:45:22 -0500

    Documentacion Metodos
    Se hace documentacion de los metodos usados, se hace correcion del ingreso de los datos date en Main.java

--- KataBank/src/main/java/Account/Account.java
+++ KataBank/src/main/java/Account/Account.java
@@ -5,18 +5,40 @@ public class Account {
     double totalBalance = 0;
     String date = "";
 
+
+    /**
+     * metodo para ingresar valor a balance total ingresando fecha
+     *
+     * @param amount
+     * @param date
+     */
     public void deposit(double amount, String date) {
         totalBalance += amount;
         this.date = date;
     }
-    public void withdraw(double amount,  String date){
+
+    /**
+     * metodo para restar valor a su balance totall, ingresando fecha
+     *
+     * @param amount
+     * @param date
+     */
+    public void withdraw(double amount, String date) {
         totalBalance -= amount;
     }
-    public void printStatements(double credit,double debit) {
+
+    /**
+     * metodo para imprimir estado de cuenta
+     *
+     * @param credit
+     * @param debit
+     */
+
+    public void printStatements(double credit, double debit) {
         totalBalance = credit - debit;
         System.out.println("Este es su estado de cuenta:"
-                +"\n" + "Credit: " + credit + " || 10/01/2021 || 13/01/2021"
-                +"\n" + "Debit: " + debit + " || 14/01/2021 "
-                +"\n"+  "Balance: " + totalBalance);
+                + "\n" + "Credit: " + credit + " || 10/01/2021 || 13/01/2021"
+                + "\n" + "Debit: " + debit + " || 14/01/2021 "
+                + "\n" + "Balance: " + totalBalance);
     }
 }
--- KataBank/src/main/java/Main/Main.java
+++ KataBank/src/main/java/Main/Main.java
@@ -8,10 +8,13 @@ public class Main {
          * Creacion Objeto account de tipo Account
          */
         Account account;
-        account =new Account();
+        account = new Account();
 
-        account.deposit(1000,("10/01/2021"));
-        account.deposit(2000, ("13/01/2021"));
+        /**
+         * Peticiones del ejercicio
+         */
+        account.deposit(1000, "10/01/2021");
+        account.deposit(2000, "13/01/2021");
         account.withdraw(500, "14/01/2021");
         account.printStatements(3000, 500);
     }
